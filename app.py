439869f4c0dd278182684f58b654c048dd76b646
Maks <makser.hofman@gmail.com>
2024-07-10 22:14:08 +0200

    Task 5 complete

--- app.py
+++ app.py
@@ -1,18 +1,37 @@
 from flask import Flask, render_template, redirect, url_for, request, session
-from flask_sqlalchemy import SQLAlchemy
+import sqlite3
 
 app = Flask(__name__, static_url_path='/static')
 
 registered_users = {}
 
+#conn = sqlite3.connect('website.db')
+#cursor = conn.cursor()
 
 app.secret_key = 'your_secret_key'  # Change this to a secure secret key
 
-# Dummy user data (replace with your actual user authentication mechanism)
-users = {
-    'qw': 'qw',
-    'zx': 'zx'
-}
+
+def get_user_from_db(username):
+    conn = sqlite3.connect('website.db')
+    cursor = conn.cursor()
+    cursor.execute(f'SELECT username, mass, age, height FROM users WHERE username = "{username}";')
+    output = cursor.fetchall()
+    conn.close()
+    return output[0][0], output[0][1], output[0][2], output[0][3]
+
+
+
+#Funkcjia laczy sie z serwerm. Sprawdza czy haslo i login pasuja i zwraca boolowska wartosc
+def checking_if_login_correct(login: str, password: str) -> bool:
+    conn = sqlite3.connect('website.db')
+    cursor = conn.cursor()
+    cursor.execute(f'SELECT CASE WHEN EXISTS (SELECT 1 FROM users WHERE username="{login}" AND password="{password}") THEN 1 ELSE 0 END;')
+    output = cursor.fetchall()
+    conn.close()
+    if output[0][0] == 1:
+        return True
+    else:
+        return False
 
 @app.route('/')
 def home():
@@ -36,11 +55,15 @@ def register():
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
-        username = request.form['username']
+        login = request.form['username']
         password = request.form['password']
-        if username in users and users[username] == password:
+        if checking_if_login_correct(login, password):
             session['logged_in'] = True
+            username, mass, age, height = get_user_from_db(login)
             session['username'] = username
+            session['mass'] = mass
+            session['age'] = age
+            session['height'] = height
             return redirect(url_for('user_page'))
         else:
             return 'Invalid username or password'
@@ -72,6 +95,8 @@ def history():
 @app.route('/user_page')
 def user_page():
     if 'logged_in' in session:
+        user = session['username']
+        print(user)
         return render_template('user_page.html')
     else:
         return redirect(url_for('login'))
@@ -81,3 +106,4 @@ def user_page():
 
 if __name__ == '__main__':
     app.run(debug=True)
+
