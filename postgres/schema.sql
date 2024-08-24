fd1988a78dc5986a4d6f288f58336f8401e633ee
Maks <makser.hofman@gmail.com>
2024-08-24 22:24:35 +0200

    docker finished. NOT TESTED

--- postgres/schema.sql
+++ postgres/schema.sql
@@ -1,35 +1,44 @@
 CREATE TABLE IF NOT EXISTS users (
-        username TEXT NOT NULL UNIQUE,
-        email TEXT NOT NULL UNIQUE,
-        password TEXT NOT NULL,
-        mass INTEGER,
-        age INTEGER,
-        height INTEGER,
-        gender TEXT,
-        goal TEXT,
-        account_created_date DATE,
-        activity_level TEXT,
-        best_streak INTEGER,
-        current_streak INTEGER,
-        days_when_on_site INTEGER,
-        added_produckts INTEGER,
-        pr_chest INTEGER,
-        last_day_user_checked_site DATE,
-        PRIMARY KEY (email)
-       );
-            CREATE TABLE IF NOT EXISTS products (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            name TEXT NOT NULL UNIQUE,
-            company TEXT NOT NULL,
-            shop TEXT NOT NULL,
-            mass INTEGER NOT NULL,
-            energy_value FLOAT NOT NULL,
-            fat FLOAT NOT NULL,
-            saturated_fat FLOAT NOT NULL,
-            carbohydrates FLOAT NOT NULL,
-            sugars FLOAT NOT NULL,
-            fiber FLOAT NOT NULL,
-            proteins FLOAT NOT NULL,
-            salts FLOAT NOT NULL,
-            rating FLOAT NOT NULL
-            )
+    username TEXT NOT NULL UNIQUE,
+    email TEXT NOT NULL UNIQUE,
+    password TEXT NOT NULL,
+    mass INTEGER,
+    age INTEGER,
+    height INTEGER,
+    gender TEXT,
+    goal TEXT,
+    account_created_date DATE,
+    activity_level TEXT,
+    best_streak INTEGER,
+    current_streak INTEGER,
+    days_when_on_site INTEGER,
+    added_products INTEGER, 
+    pr_chest INTEGER,
+    last_day_user_checked_site DATE,
+    PRIMARY KEY (email)
+);
+
+CREATE TABLE IF NOT EXISTS products (
+    id SERIAL PRIMARY KEY, 
+    name TEXT NOT NULL UNIQUE,
+    company TEXT NOT NULL,
+    shop TEXT NOT NULL,
+    mass INTEGER NOT NULL,
+    energy_value REAL NOT NULL, 
+    fat REAL NOT NULL,
+    saturated_fat REAL NOT NULL,
+    carbohydrates REAL NOT NULL,
+    sugars REAL NOT NULL,
+    fiber REAL NOT NULL,
+    proteins REAL NOT NULL,
+    salts REAL NOT NULL,
+    rating REAL NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS progress (
+    user_id TEXT,
+    progress_update INTEGER NOT NULL,
+    progress_update_date DATE NOT NULL,
+    progress_type TEXT NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(email)
+);
