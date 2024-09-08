c8a36cd726ebbaac62c896a836cbf35b149d65c2
Maks <makser.hofman@gmail.com>
2024-09-08 22:10:36 +0200

    task 42

--- stop.sh
+++ stop.sh
@@ -1,9 +1,3 @@
-#!/usr/bin/env bash
-
-PROJECT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-PROJECT_NAME="$(basename "${PROJECT_DIR}")"
-
-# stop previous running containers
-if [ -n "$(docker ps -a | grep "${PROJECT_NAME}")" ]; then
-  docker ps -a | grep "${PROJECT_NAME}" | awk '{print $1}' | xargs docker stop
-fi
+docker-compose down -v
+docker rmi kcal-app-image:1.0
+docker rmi kcal-postgres:1.0
--- str.sh
+++
@@ -1,4 +0,0 @@
-docker build -t kcal-postgres:1.0 ./postgres
-docker build -t kcal-app-image:1.0 ./site
-docker-compose up -d
-docker ps
